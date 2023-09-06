4ca464d45e005f80ee2af46c2245ccb23feeec77
theodorsm <theodor@midtlien.com>
2023-09-06 11:54:27 +0000

    WIP custom NF (not supported by open5gs)

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 
+	"golang.org/x/net/http2"
+
+	"github.com/google/uuid"
 	"gopkg.in/yaml.v2"
 )
 
-type Response struct {
-	Message string `json:"message"`
-}
-
 type TestcaseConfig struct {
+	Nrf       string    `yaml:nrf`
 	Imsi      string    `yaml:imsi`
 	Testcases Testcases `yaml:testcases`
 }
@@ -46,23 +50,17 @@ type Offset struct {
 	Value  string `yaml:value`
 }
 
+var config TestcaseConfig
+var localAddr string
+var localPort string
+
 func main() {
-	http.HandleFunc("/ninf/v1/config", handleRequest)
-	log.Fatal(http.ListenAndServe("127.0.13.37:7777", nil))
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
+	localAddr = "127.0.13.37"
+	localPort = ":7777"
 	data, err := ioutil.ReadFile("testcases.yaml")
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
-
-	var config TestcaseConfig
 
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
@@ -70,7 +68,97 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(config)
+
+	registerToNrf()
+	http.HandleFunc("/ninf/v1/config", handleConfigRequest)
+	http.HandleFunc("/ninf/v1/imsi", handleImsiRequest)
+	log.Fatal(http.ListenAndServe(localAddr+localPort, nil))
+}
+
+func registerToNrf() {
+	id := uuid.New()
+	httpurl := fmt.Sprintf("http://%s/nnrf-nfm/v1/nf-instances/%s", config.Nrf, id.String())
+	fmt.Println("HTTP JSON URL:", httpurl)
+
+	type NfRegister struct {
+		NfInstanceId  string   `json:"nfInstanceId"`
+		NfType        string   `json:"nfType"`
+		NfStatus      string   `json:"nfStatus"`
+		Ipv4addresses []string `json:"ipv4Addresses"`
+		/*
+			AllowedNfTypes             []string `json:"allowedNfTypes"`
+			Priority                   uint     `json:"priority"`
+			Capacity                   uint     `json:"capacity"`
+			Load                       uint     `json:"load"`
+			NfProfileChangesSupportInd bool     `json:"nfProfileChangesSupportInd"`
+		*/
+	}
+
+	//jjsonData := NfRegister{id.String(), "INF", "REGISTERED", []string{localAddr}, []string{"AMF"}, 0, 100, 0, true}
+	jsonData := NfRegister{id.String(), "CUSTOM_INF", "REGISTERED", []string{localAddr}}
+
+	marshalled, err := json.Marshal(jsonData)
+	fmt.Println(string(marshalled))
+	if err != nil {
+		log.Fatalf("impossible to marshall jsonData: %s", err)
+	}
+	req, err := http.NewRequest("PUT", httpurl, bytes.NewReader(marshalled))
+	if err != nil {
+		log.Fatalf("impossible to build request: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/json;")
+
+	// Create an HTTP/2 Transport with Prior Knowledge
+	transport := &http2.Transport{
+		AllowHTTP: true,
+		DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+			return net.Dial(network, addr)
+		},
+	}
+
+	client := &http.Client{Transport: transport}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("impossible to send request: %s", err)
+	}
+	log.Printf("status Code: %d", res.StatusCode)
+	defer res.Body.Close()
+	// read body
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("impossible to read all body of response: %s", err)
+	}
+	log.Printf("res body: %s", string(resBody))
+
+}
+
+func handleConfigRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	response := config
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleImsiRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	type ImsiResponse struct {
+		Imsi string `json:"imsi"`
+	}
+
+	response := ImsiResponse{Imsi: config.Imsi}
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(response); err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module inf
 
 go 1.18
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/google/uuid v1.3.1
+	golang.org/x/net v0.14.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require golang.org/x/text v0.12.0 // indirect
